798c126455c2e306ccec845619ac3db9071d0e8c
Natan Martins <natan.n.martins20@gmail.com>
2023-10-16 23:50:45 -0300

    criando agente de aeroporto

--- src/trafegoaereo/agents/Aeroporto.java
+++ src/trafegoaereo/agents/Aeroporto.java
@@ -3,11 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package trafegoaereo.agents;
+import java.util.List;
 
 /**
  *
  * @author Natan
  */
 public class Aeroporto {
+    List<Aeronave> aeronaves;
+    int quantidadePessoas = 0;
+    String enderecoAeroporto = "";    
+    boolean possuiAviaoNaPista = false;
+    
+    
+    public Aeroporto(){}
+
+    public Aeroporto(List<Aeronave> aeronaves, int quantidadePessoas, String enderecoAeroporto, boolean possuiAviaoNaPista)
+    {
+        //this.passageiros = passageiros;
+        this.aeronaves = aeronaves;
+        this.quantidadePessoas = quantidadePessoas;
+        this.enderecoAeroporto = enderecoAeroporto;
+        this.possuiAviaoNaPista = possuiAviaoNaPista;
+    }
     
 }
